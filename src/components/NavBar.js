02bdbfd62a47de0199dbd60c8f52967452c9e986
Sheila Seruya <Sheilaseruya@gmail.com>
2023-04-26 10:52:02 +0300

    done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -3,12 +3,14 @@ import React from "react";
 function NavBar({}) {
   const links = ["home", "about", "projects"];
 
-  return <nav>
-    {links.map((link, index)=>(
-      <a key={index} href={`/${link}`}>{link}</a>
-    )
-    )}
-  </nav>;
+  return (
+  <nav>
+   <a href="#home">home</a>
+   <a href="#about">about</a>
+   <a href="#projects">projects</a>
+
+  </nav>
+  );
 }
 
 export default NavBar;
--- src/components/ProjectList.js
+++ src/components/ProjectList.js
@@ -7,7 +7,16 @@ function ProjectList({ projects }) {
     <div id="projects">
       <h2>My Projects</h2>
       <div id="project-list">
-        {}
+        {
+          projects.map(project => (
+            <ProjectItem
+            key={project.id}
+            name={project.name}
+            about={project.about}
+            technologies={project.technologies}
+            />
+          ))
+        }
       </div>
     </div>
   );
